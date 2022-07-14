aa8e98b83c60b9e1f0bb11b8e0fbb59a5e1407ad
dianaasmarani <muhammadamsari15@gmail.com>
2022-07-14 09:15:06 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Muhammad Amsari Lubis
+            Saya tinggal di Siboris Dolok, Barumun Tengah, 
+            Padang Lawas. Olahraga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
